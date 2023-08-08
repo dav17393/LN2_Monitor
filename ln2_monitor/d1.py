556d661c3e61741f16134e073b20c420a2dc7387
dav17393 <137222876+dav17393@users.noreply.github.com>
2023-08-08 11:51:20 -0600

    Update d1.py

--- ln2_monitor/d1.py
+++ ln2_monitor/d1.py
@@ -27,6 +27,10 @@ class SCL():
     
     if __name__ == "__main":
         main()
+    
+   
+     
+       
 
     def getdata(self):
         try:
@@ -42,8 +46,8 @@ class SCL():
         
      
     def setweight(self):
-        self.weighmax = float(input("What is the Maximum Weight(kg) of the Cryostat?:"))
-        self.weighmin = float(input("What is the Minumum Weight(kg) of the Cryostat?:"))
+        self.weighmax = float(input("What is the Maximum Weight(kg) of the LN2 Trap?:"))
+        self.weighmin = float(input("What is the Minumum Weight(kg) of the LN2 Trap?:"))
     def getpercentfull(self):
         self.per= None
         while self.per == None:
@@ -69,34 +73,42 @@ class SCL():
                 continue
         print(self.kg)       
     
-    def plot(self, interval=1000):
+    def plot(self, interval=1000000):
         x_data, y_data = [], []
-        now = datetime.now()
-        date_time = now.strftime("%Y_%m_%d, Dymo")
-        date_timesec = now.strftime("%Y/%m/%d, %H:%M:%S")
-        self.tfile = open(date_time+'.txt', 'a')
+        date_time = datetime.now().strftime("%Y_%m_%d Dymo")
+        self.tfile = open(date_time+ '.txt', 'a')
     
         figure = pyplot.figure()
         line, = pyplot.plot_date(x_data, y_data, '-')
         self.interval = interval
-        pyplot.title('Cryostat fullness')
-        pyplot.ylabel('Percent full')
+        pyplot.title('LN2 Trap Fullness')
+        pyplot.ylabel('Percent Full')
+        pyplot.xlabel('Time')
        
         
         def update(frame):
             self.getpercentfull()
+            ax = pyplot.gca()
             x_data.append(datetime.now())
             y_data.append(self.per)
-            items = str(date_timesec)+","+" "+str(round(self.per,2))+"%"+"\n"
+            items = str(datetime.now().strftime("%H:%M:%S")) +","+" "+str(round(self.per,2))+"%"+"\n"
             self.tfile.write(items)
             line.set_data(x_data, y_data)
             figure.gca().relim()
             figure.gca().autoscale_view()
+            pyplot.plot(x_data, y_data, '-', color='blue')
+            if self.per > 10:
+                ax.set_facecolor('white')
+            if self.per < 20:
+                ax.set_facecolor('red')
             return line,
-  
-        animation = FuncAnimation(figure, update, interval=self.interval)
-      
+
+ 
+        animation = FuncAnimation(figure, update, interval=self.interval/1000)
+        #pyplot.figure(figsize=(12,8))
+        pyplot.xticks(rotation = 30)
         pyplot.show()
+
         self.tfile.close()
       
   

6cf2ade16b989a279a55cdf185bf9d2b0e9ca20b
dav17393 <137222876+dav17393@users.noreply.github.com>
2023-08-09 14:04:15 -0600

    Update d1.py

--- ln2_monitor/d1.py
+++ ln2_monitor/d1.py
@@ -9,7 +9,6 @@ from matplotlib import pyplot
 from matplotlib.animation import FuncAnimation
 from random import randrange
 
-
 class SCL():
 
     def __init__(self):
@@ -19,19 +18,43 @@ class SCL():
         self.device.set_configuration()
         self.endpoint = self.device[0][(0,0)][0]
         self.data = None
-        self.weighmax = 28.2588
-        self.weighmin = 10.97694
-    
+        try:
+            file=open("config.txt", "r")
+            lines = file.readlines()
+            weightmax_string = lines[0].split(":")[1].split("kg")[0]
+            weighmin_string = lines[1].split(":")[1].split("kg")[0]
+            self.weighmax = float(weightmax_string)
+            self.weighmin = float(weighmin_string)
+        except:
+            print("no default weight found, weight is now set to a standard LN2 trap(max:28.2588, min:10.97694)")
+            self.weighmax = 29.4588
+            self.weighmin = 12.17694
+    def plotdata(self):
+        date_time = datetime.now().strftime("%Y_%m_%d Dymo")
+        x_ticks = []
+        y_ticks = []
+        self.filename = (input("What day's data would you like to plot(YEAR_MONTH_DATE):"))
+        file = open(self.filename+' Dymo'+ '.txt', 'r')
+        liness = file.readlines()
+        x_values = []
+        y_values = []
+        for i in range(0,len(liness)):
+            y_values.append(float(liness[i].split(",")[1].split("%")[0]))
+            x_values.append(datetime.strptime((liness[i].split(",")[0]),"%H:%M:%S") )
+       
+        line, = pyplot.plot_date(x_values, y_values, '-')
+        pyplot.title('LN2 Trap Fullness(based on the txt file)')
+        pyplot.ylabel('Percent Full')
+        pyplot.xlabel('Time')
+        pyplot.xticks(rotation = 30)
+        pyplot.show()
+
     def main(self):
         self.plot()
     
     if __name__ == "__main":
         main()
     
-   
-     
-       
-
     def getdata(self):
         try:
            self.data = self.device.read(self.endpoint.bEndpointAddress,self.endpoint.wMaxPacketSize)
@@ -48,6 +71,16 @@ class SCL():
     def setweight(self):
         self.weighmax = float(input("What is the Maximum Weight(kg) of the LN2 Trap?:"))
         self.weighmin = float(input("What is the Minumum Weight(kg) of the LN2 Trap?:"))
+        self.config = input("Would you like to save these values in a txt file?(y/n):")
+        if str(self.config) == 'y':
+            self.tfile = open('config'+ '.txt', 'a')   
+            items = "max_weight" + ":" + str(self.weighmax) + "kg" + "\n" + "min_weight" + ":" + str(self.weighmin)+"kg"
+            self.tfile.truncate(0)
+            self.tfile.write(items)
+            self.tfile.close()
+        if str(self.config) == 'n':
+            print("Ok")
+    
     def getpercentfull(self):
         self.per= None
         while self.per == None:
@@ -105,13 +138,12 @@ class SCL():
 
  
         animation = FuncAnimation(figure, update, interval=self.interval/1000)
-        #pyplot.figure(figsize=(12,8))
         pyplot.xticks(rotation = 30)
         pyplot.show()
 
         self.tfile.close()
       
-  
+   
     def __str__(self):
         return f"{self.data}"
         
